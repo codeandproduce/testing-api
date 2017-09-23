0c23a1a8c052b41fb5c61191e9cd8b7923ada1c3
Chan Woo Kim <chanwookim@Chans-MacBook-Pro.local>
2017-09-23 14:32:48 -0400

    init

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ io.on('connection', (socket)=>{
   console.log('connected');
 });
 
-const publichPath = path.join(__dirname, '/public');
+const publicPath = path.join(__dirname, '/public');
 app.use('/', express.static(publicPath));
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -28,6 +28,9 @@ app.get('/', (req, res)=>{
   res.render('index');
 });
 
+app.post('/api', (req,res)=>{
+
+});
 server.listen(port, ()=>{
   console.log("Port is up and running at",port);
 });
